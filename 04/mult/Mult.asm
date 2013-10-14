6be412a1ab0839330b96bada18bfd4144cefca78
Polina Giralt <polina.giralt@gmail.com>
2013-10-14 18:47:08 -0400

    Mult.asm - Multiplies R0 and R1 and stores the result in R2. (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -13,6 +13,8 @@
 
 	@i     // i refers to some mem. location
 	M=1    // i=0
+	@R2
+	M=0    //R2=0
 
 (LOOP)
 // loop will increment i until R1 = i
@@ -28,11 +30,12 @@
 	@R0
 	D=M    // D=R0
 	@R2
-	M=D+M  // product=product+R0
+	M=D+M  // R2=R0+R2 (adds R0)
 	@i
 	M=M+1  // i=i+1
 	@LOOP
 	0;JMP  // Goto LOOP
+
 (END)
 	@END
 	0;JMP  // Infinite loop
