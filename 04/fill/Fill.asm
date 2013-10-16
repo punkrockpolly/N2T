ad9dacac0c850232cac31d4ef94f697453dccf5a
Polina Giralt <polina.giralt@gmail.com>
2013-10-16 00:10:59 -0400

    success! code functional for Fill.asm // Runs an infinite loop that listens to the keyboard input. // When a key is pressed (any key), the program blackens the screen, // i.e. writes "black" in every pixel. When no key is pressed, the // program clears the screen, i.e. writes "white" in every pixel.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,61 +9,75 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
-	@SCREEN
-	D=A
-	@pixel 
-	M=D    // D is current pixel (starts at SCREEN, then iterates each loop)
 	@8192  // number of pixels on a screen divided by 16-bit address (256 x 512 / 16)
 	D=A    // D = total number of pixels
 	@lastpixel
 	M=D    // assigns lastpixel with value of num pixels
 
 (LOOP)
+	@SCREEN
+	D=A
+	@pixel 
+	M=D    // D is current pixel (starts at SCREEN, then iterates each loop)
 	@i     // i refers to some mem. location
 	M=0    // i=0
 	@KBD
 	D=M    // D=keyboard input
-	@color
-	M=0
-	@COLORLOOP
-	D;JEQ  // if no keyboard input, then skip to COLORLOOP
-	@color
-	M=-1   // otherwise, change color to black (-1)
+	@COLORWHITE
+	D;JEQ  // if no keyboard input, then skip to COLORWHITE
 
-(COLORLOOP)
-	@lastpixel
-	D=M    // D is the last pixel
-	@i
-	D=D-M  // D is lastpixel - iteration
-	@LOOP
-	D;JLE  // If(run out of pixels): < = 0 goto LOOP		
-	
-	@i
-	M=M+1  // i=i+1 - iterates each loop
-	D=M
-	@pixel
-	M=M+1  // iterates pixel
-
+(COLORBLACK)	
 	@color
-	D=M
-	@WHITE
-	D;JEQ  // If color is white, jump to WHITE
-	@BLACK
-	D;JNE  // otherwise, jump to BLACK
+	M=-1   // change color to black (-1)
 
-(BLACK)
 	@pixel
 	D=M    // D is current pixel (starts at SCREEN, then iterates each loop)
 	A=D    // address = D
 	M=-1   // sets current address equal to -1 (black)
+	@pixel
+	M=M+1  // current pixel will iterate by i
+	
+	@i
+	M=M+1  // i=i+1 - iterates each loop
+	D=M
+	@lastpixel
+	D=M-D  // D is difference between lastpixel and i
 	@LOOP
-	0;JMP  // Goto LOOP
+	D;JLE  // If(run out of pixels): < = 0 goto LOOP 
+	
+	@KBD
+	D=M    // D=keyboard input
+	@COLORBLACK
+	D;JGT  // if keyboard input, goto COLORBLACK
 
-(WHITE)
+// this code only gets executed when switching from Black to White	
+	@SCREEN 
+	D=A
+	@pixel // reset pixel
+	M=D    // D is current pixel (starts at SCREEN, then iterates each loop)
+
+(COLORWHITE)
+	@color
+	M=0    // sets current color equal to 0 (white)
+	
 	@pixel
 	D=M    // D is current pixel (starts at SCREEN, then iterates each loop)
 	A=D    // address = D
-	M=-1   // sets current address equal to 0 (white)
-	@LOOP
-	0;JMP  // Goto LOOP
+	M=0    // sets current address equal to 0 (white)	
+	@pixel
+	M=M+1  // current pixel will iterate by i
 
+	@i
+	M=M+1  // i=i+1 - iterates each loop
+	D=M
+	@lastpixel
+	D=M-D  // D is difference between lastpixel and i
+	@LOOP
+	D;JLE  // If(run out of pixels): < = 0 goto LOOP 
+	
+	@KBD
+	D=M    // D=keyboard input
+	@LOOP
+	D;JNE  // if keyboard input, goto LOOP
+	@COLORWHITE
+	0;JMP  // Goto COLORBLACK
